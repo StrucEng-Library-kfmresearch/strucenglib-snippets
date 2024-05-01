f96075fad3d6d86d04728ef2d3d64906c0403cf8
SophiaKuhn <98028544+SophiaKuhn@users.noreply.github.com>
2024-05-01 11:17:23 +0200

    Update __init__.py

--- strucenglib/prepost_functions/__init__.py
+++ strucenglib/prepost_functions/__init__.py
@@ -6,4 +6,5 @@ from strucenglib.prepost_functions.plot_nr_elem import *
 from strucenglib.prepost_functions.plot_nr_nodes import * 
 from strucenglib.prepost_functions.area_load_generator_elements import *
 from strucenglib.prepost_functions.Normalspurbahnverkehr_load_generator import *
+from strucenglib.prepost_functions.earthPressure_load_generator import *
 from strucenglib.prepost_functions.verification import *
